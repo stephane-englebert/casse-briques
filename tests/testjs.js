261a76c58a92114481a0b0f450a298679c8e88a2
stephane-englebert <stephane.englebert@gmail.com>
2022-05-27 16:55:10 +0200

    last commit

--- tests/testjs.js
+++ tests/testjs.js
@@ -14,13 +14,22 @@ function draw(){
     context.fillStyle = '#ff8080';
     context.fillRect(rectX, rectY, 150, 100);
 }
+function drawCircle(x,y){
+    context.beginPath();
+    context.strokeStyle = '#0099b0';
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+    context.fillStyle = '#0099b0';
+    context.fill();
+    context.closePath();
+}
 function init(){
     // Get a reference to the canvas
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
 
     draw();
-    window.requestAnimationFrame(gameLoop);
+    drawCircle(600,200);
+    //window.requestAnimationFrame(gameLoop);
 }
 "use strict";
 let canvas;
